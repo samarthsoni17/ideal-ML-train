5e949677fa65dba03bcf2220f82eaddd04b3bf94
Samarth Soni <s.soni87@yahoo.com>
2025-10-01 13:15:42 +0800

    Configs | hpc | Add git/unix settings

--- conda_hpc.bash
+++ conda_hpc.bash
@@ -70,7 +70,9 @@ conda activate /$WORK/$USER/conda/envs/ai1008
 (ai1008) [ ]$ conda config --set channel_priority strict
 #-y = “yes to all prompts” (helpful for non-interactive environments like HPC)
 (ai1008) [ ]$ conda install -y jupyterlab ipython ipykernel numpy pandas scipy scikit-learn matplotlib tqdm ipywidgets pyarrow plotly
-conda install -y kaleido imageio
+(ai1008) [ ]$ conda install -y kaleido imageio
+(ai1008) [ ]$ plotly_get_chrome
+#The Chrome executable is now located at: /hpctmp/e1554287/conda/envs/ai1008/lib/python3.13/site-packages/choreographer/cli/browser_exe/chrome-linux64/chrome
 
 # Make and register a named kernel (lives in $HOME; survives purges of $WORK and $SCRATCH; Jupyter sees this env by name)
 python -m ipykernel install --user --name ai1008 --display-name "ai1008 (Atlas)"
@@ -95,3 +97,72 @@ source samstartup.sh
 #some unix setups
 vim ~/.vimrc
 colorscheme desert
+
+vim ~/.bashrc
+# User specific aliases and functions
+alias ls='ls --color=auto'
+export PS1="\[\033[1;32m\]\u@\h:\[\033[1;34m\]\w\[\033[0m\]$ "
+alias ll='ls -lhF'
+alias gst='git status'
+#some useful flags for ls
+#    -l	Long format: shows permissions, owner, group, size, date, and filename
+#    -a	All files: includes hidden files (those starting with .)
+#    -r	Reverse order: reverses the sorting order
+#    -t	Sort by time: sorts by last modification time (newest first, unless reversed)
+#    -h	Human-readable: shows sizes as 1K, 234M, etc.
+#    -F	Classify: adds / for directories, * for executables, @ for symlinks, etc.
+
+#git and sshkey setup
+ssh-keygen -t ed25519 -C "s.soni87@yahoo.com"
+#  Enter passphrase:
+
+eval "$(ssh-agent -s)"
+#  Agent pid 133714
+
+ssh-add ~/.ssh/id_ed25519
+#  Enter passphrase for /home/svu/e1554287/.ssh/id_ed25519:
+#  Identity added: /home/svu/e1554287/.ssh/id_ed25519 s.soni87@yahoo.com
+
+eval "$(ssh-agent -s)"
+#  Agent pid 133775
+
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/id_ed25519
+vim ~/.ssh/config
+#Add the following"
+#  Host github.com
+#    HostName ssh.github.com
+#    Port 443
+#    User git
+#    IdentityFile ~/.ssh/id_ed25519
+#    IdentitiesOnly yes
+#    ServerAliveInterval 60
+#    ServerAliveCountMax 3
+#    StrictHostKeyChecking yes
+#    UserKnownHostsFile ~/.ssh/known_hosts
+
+ssh -T git@github.com
+#  Warning: Permanently added '[[ssh.github.com](http://ssh.github.com/)]:443,[20.205.243.160]:443' (ECDSA) to the list of known hosts.
+#  Enter passphrase for key '/home/.ssh/id_ed25519':
+#  Hi samarthsoni17! You've successfully authenticated, but GitHub does not provide shell access.
+
+#check updated list of known hosts:
+cat ~/.ssh/known_hosts
+
+cd $HOME/projects
+git clone git@github.com:samarthsoni17/ideal-ML-train.git
+
+git config --global user.name "Samarth Soni"
+git config --global user.email "s.soni87@yahoo.com"
+git config --global push.default simple
+git config --global color.ui auto
+#to go beyond git default colour scheme:
+git config --global color.status true
+# OPTIONAL Set custom colors for status output:
+git config --global color.status.added "green bold"
+git config --global color.status.changed "yellow bold"
+git config --global color.status.untracked "red bold"
+git config --global color.diff.meta "magenta bold"
+git config --global color.branch.current "yellow reverse"
+#to see current color settings:
+git config --get-regexp color
